ea056845dbb9d013aeec94578d0ddbd94d3d9d96
Franciele França <92552849+francielefranca@users.noreply.github.com>
2024-01-18 18:16:30 -0300

    add usuarios-objeto.js
    training objects in js

---
+++ usuarios-objeto.js
@@ -0,0 +1,8 @@
+let usuarios = [
+    {nome: "Rachel Olie", cargo:"Juiza", status:"Ativo"},
+    {nome: "Harry Hole", cargo:"Investigador", status:"Ativo"},
+    {nome: "Jack Harrison", cargo:"Detetive", status:"Ativo"},
+]
+
+console.log(usuarios)
+console.log(usuarios[1].cargo)
