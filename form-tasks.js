80d16c01435eec1db1b03921ccf6a7dd68413805
Franciele França <92552849+francielefranca@users.noreply.github.com>
2024-01-20 20:08:13 -0300

    add form-tasks.js utilizando useState com array e mostrando na tela

---
+++ form-tasks.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+function App(){
+  const [input, setInput] = useState('')
+  const [tasks, setTasks] = useState([
+    'Pagar conta de luz',
+    'Estudar react',
+  ]) 
+
+
+  function handleRegister(e){
+    e.preventDefault();
+
+    setTasks([...tasks, input]);
+    setInput('');
+
+  }
+
+  return(
+    <div>
+      <h1>Registrando Tarefas</h1>
+
+      <form onSubmit={ handleRegister}>
+
+        <label>Nome da task:</label><br/>
+        <input placeholder='Digite uma task'
+        value={input}
+        onChange={ (e)=> setInput(e.target.value)}
+        ></input><br/>
+
+        <button type='submit'>Registrar task</button>
+      </form>
+      <br/><br/>
+
+      <ul>
+        {tasks.map( task => (
+          <li key={task}>{task}</li>
+        ))}
+      </ul>
+      
+    </div>
+  )
+}
+
+export default App;
